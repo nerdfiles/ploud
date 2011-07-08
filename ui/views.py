133589c499268ab3910a0efcd125e0cd6d9dd972
Aaron Alexander <nerdfiles@gmail.com>
2011-07-08 16:54:20 -0500

    first

---
+++ ui/views.py
@@ -0,0 +1,20 @@
+# Create your views here.
+from django.http import HttpResponse
+from django.template import Context, loader
+from django.template.defaultfilters import slugify
+from django.shortcuts import render_to_response, get_list_or_404, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.core.mail import send_mail, EmailMultiAlternatives
+from django.conf import settings
+
+def index(request):
+    return render_to_response('ui/index.html')
+
+def home(request):
+    return render_to_response('ui/home.html')
+    
+def dashboard(request):
+    return render_to_response('ui/dashboard.html')
+    
+def reset_password(request):
+    return render_to_response('ui/reset_password.html')
---
+++ urls.py
@@ -0,0 +1,28 @@
+from django.conf.urls.defaults import *
+from django.conf import settings
+from django.contrib import admin
+from django.views.generic.simple import direct_to_template
+from os import path
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Base
+    url(r'^$', 'ui.views.home'),
+    
+    # Standard Pages
+    url(r'^home/$', 'ui.views.home'),
+    url(r'^dashboard/$', 'ui.views.dashboard'),
+    url(r'^reset-password/$', 'ui.views.reset_password'),
+    
+    # Admin Pages
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns.extend(patterns(
+        '',
+        (r'^_assets/(?P<path>.*)$', 'django.views.static.serve', {'document_root':settings.MEDIA_ROOT}),
+))
