b8d98c2b29a4b9fee394b9ab9cfa79f7f059d663
Aaron Alexander <nerdfiles@gmail.com>
2011-07-12 01:13:26 -0500

    statuses, more cloudy things

--- _assets/_js/global.js
+++ _assets/_js/global.js
@@ -1,3 +1,66 @@
-$script.ready('jquery', function() {
+$script.ready(['jquery', 'colorbox'], function() {
+
     
+    var Ploud = {
+        
+        setUpColorbox: function() {
+        
+            $('.colorbox').each(function() {
+            
+                var $self = $(this),
+                    href = '#'+$(this).attr('href').replace(/\//g, '');
+                    
+                $self.colorbox({
+                    onOpen: function() {
+                        $('#colorbox').hide();
+                    },
+                    onLoad: function() {
+                        $('#colorbox').fadeIn('slow');    
+                    },
+                    transition: 'none',
+                    width: '50%',
+                    height: '50%',
+                    inline: true, 
+                    href: href
+                });
+            
+            });
+        
+        },
+        
+        kbdOnButtons: function() {
+        
+            $('.button').each(function() { 
+                var $self = $(this);
+                
+                $self.bind('keydown keyup', function(e) {
+                    
+                    if ( e.type === 'keydown' ) {
+                        $self.css({
+                            position: 'relative',
+                            top: '1px'
+                        });
+                    } else if ( e.type === 'keyup' ) {
+                    
+                        $self.css({
+                            top: '0px'
+                        });
+                    
+                    }
+                    
+                });
+            });
+            
+        },
+        
+        init: function() {
+            this.setUpColorbox();
+            this.kbdOnButtons();
+        }    
+    
+    }; 
+    
+    Ploud.init();
+    
+    // End dependency check
 });
