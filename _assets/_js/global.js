28761fa7822a131b15e3cbb924c98df7eaf6def6
Aaron Alexander <nerdfiles@gmail.com>
2011-07-12 10:00:27 -0500

    actions menus

--- _assets/_js/global.js
+++ _assets/_js/global.js
@@ -1,5 +1,34 @@
 $script.ready(['jquery', 'colorbox'], function() {
 
+    /**
+     * jQuery toggleFade
+     *
+     * @author      nerdfiles
+     * @usage 
+     *              $(selector).toggleFade(speed);
+     *
+     */
+     
+    $.fn.toggleFade = function(speed) {
+    
+        return this.each(function() {
+        
+            var $self = $(this),
+                d = $.css(this,"display");
+                
+            if ( d === "none" || d === '' ) {
+            
+                $self.fadeIn(speed);
+                
+            } else {
+            
+                $self.fadeOut(speed);
+                
+            }
+            
+        });
+        
+    } // End $.fn.toggleFade
     
     var Ploud = {
         
@@ -51,9 +80,24 @@ $script.ready(['jquery', 'colorbox'], function() {
             
         },
         
+        setUpDashboardActionsMenu: function() {
+            
+            $('.site-actions-menu').prev().bind('click', function(e) {
+                var $self = $(this),
+                    $siteActionsMenu = $self.next();
+                    
+                $self.toggleClass('active');
+                $siteActionsMenu.toggleFade('fast');
+                
+                e.preventDefault();
+            });
+            
+        },
+        
         init: function() {
             this.setUpColorbox();
             this.kbdOnButtons();
+            this.setUpDashboardActionsMenu();
         }    
     
     }; 
