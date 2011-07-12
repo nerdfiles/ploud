903ae15dfadca6d5a6004d058c737c407e5acc96
Aaron Alexander <nerdfiles@gmail.com>
2011-07-12 18:04:29 -0500

    custom select

--- _assets/_js/global.js
+++ _assets/_js/global.js
@@ -125,11 +125,62 @@ $script.ready(['jquery', 'colorbox'], function() {
             });
         },
         
+        customSelect: function() {
+        
+            $('ul.custom-select').live('click', function(e) {
+                
+                var $self = $(this);
+                
+                $self.find('li').each(function() {
+                    $(this).removeClass('hide');
+                });
+                
+                $self.find('li').bind('click', function(e) {
+                    var $li = $(this),
+                        cn = $li.attr('class');
+                        
+                    $self.parent().find('.selected').remove();
+                        
+                    $li.parent().parent().prev().find('option[value="'+cn+'"]').prop('selected', 'selected');
+                    $self.prepend('<li class="selected">'+$li.text()+'</li>');
+                    
+                    $self.find('li:gt(0)').each(function() {
+                        $(this).addClass('hide');
+                        
+                        e.stopPropagation();
+                    });
+                    
+                    $self.find('li').unbind('click');
+    
+                });
+                
+            });
+        
+            $('select.select').each(function() {
+            
+                var $self = $(this),
+                    html = [];
+                
+                $self.find('option').each(function(index) {
+                    var $option = $(this);
+                    if ( index < 1 ) {
+                        html.push('<li class="rounded-all-inputs '+$option.val()+'">'+$option.text()+'</li>');
+                    } else {
+                        html.push('<li class="rounded-all-inputs hide '+$option.val()+'">'+$option.text()+'</li>');
+                    }
+                });
+                
+                $self.after('<div class="custom-select-container" style="height: '+($self.height()+18)+'px;"><ul class="custom-select dropshadow-input rounded-all-inputs">'+html.join('')+'</ul></div>')
+            
+            });
+        },
+        
         init: function() {
             this.setUpColorbox();
             this.kbdOnButtons();
             this.toggleDashboardActionsMenu();
             this.closeActionMenus();
+            this.customSelect();
         }    
     
     }; 
